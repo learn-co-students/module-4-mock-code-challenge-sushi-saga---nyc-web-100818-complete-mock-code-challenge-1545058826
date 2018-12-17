258dbd6d602c057f42d3ec6b8b18cbcce6895550
ihollander <ianholla@gmail.com>
2018-12-17 11:33:59 -0500

    Done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -15,8 +15,7 @@ class App extends Component {
   state = {
     budget: 100,
     sushiPage: paging.firstPage,
-    sushi: [],
-    plates: []
+    sushi: []
   }
 
   componentDidMount() {
@@ -24,39 +23,40 @@ class App extends Component {
   }
 
   getSushi = () => {
-    fetch(`${API}?_page=${this.state.sushiPage}&_limit=${paging.limit}`)
+    fetch(`${API}`)
       .then(r => r.json())
-      .then(sushiData => {
-        const sushi = sushiData.map(apiSushi => {
-          if (this.state.plates.find(s => s.id === apiSushi.id)) {
-            apiSushi.eaten = true
-          }
-          return apiSushi
-        })
+      .then(sushi => {
         this.setState({ sushi })
       })
   }
 
+  get displaySushi() {
+    const startIndex = (this.state.sushiPage - 1) * paging.limit
+    const endIndex = startIndex + paging.limit
+    return this.state.sushi.slice(startIndex, endIndex)
+  }
+
+  get plates() {
+    return this.state.sushi.filter(s => s.eaten)
+  }
+
   onMoreClick = () => {
     const sushiPage = this.state.sushiPage + 1 > paging.lastPage ? paging.firstPage : this.state.sushiPage + 1
-    this.setState({
-      sushiPage
-    }, this.getSushi)
+    this.setState({ sushiPage })
   }
 
   onPrevClick = () => {
     const sushiPage = this.state.sushiPage - 1 < paging.firstPage ? paging.lastPage : this.state.sushiPage - 1
-    this.setState({
-      sushiPage
-    }, this.getSushi)
+    this.setState({ sushiPage })
   }
 
   onEat = (sushi) => {
+    if (sushi.eaten) return
+    
     if (this.state.budget - sushi.price >= 0) {
       const newSushi = [...this.state.sushi].map(s => s === sushi ? { ...s, eaten: true } : s)
       this.setState({
         sushi: newSushi,
-        plates: [...this.state.plates, sushi],
         budget: this.state.budget - sushi.price
       })
     } else {
@@ -72,8 +72,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SushiContainer onEat={this.onEat} sushi={this.state.sushi} onMoreClick={this.onMoreClick} onPrevClick={this.onPrevClick} />
-        <Table onWalletSubmit={this.onWalletSubmit} plates={this.state.plates} budget={this.state.budget} />
+        <SushiContainer onEat={this.onEat} sushi={this.displaySushi} onMoreClick={this.onMoreClick} onPrevClick={this.onPrevClick} />
+        <Table onWalletSubmit={this.onWalletSubmit} plates={this.plates} budget={this.state.budget} />
       </div>
     );
   }
