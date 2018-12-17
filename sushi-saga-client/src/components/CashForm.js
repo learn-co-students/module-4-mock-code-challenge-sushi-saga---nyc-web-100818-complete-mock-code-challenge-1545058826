274977343fa9bc97730e670db13ffff2ea6355ce
asc5025 <albert.s.chun@gmail.com>
2018-12-17 14:32:47 -0500

    Done w/ bonus

---
+++ sushi-saga-client/src/components/CashForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class CashForm extends React.Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.props.addMoney(event, this.state.value)}>
+          <label>MORE $$$??</label>
+          <input type="number" name="cash" onChange={this.handleChange} value={this.state.value} />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default CashForm
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,19 @@
 import React, { Fragment } from 'react'
 
-const Sushi = (props) => {
+const Sushi = ({sushi, handleEaten}) => {
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+    { sushi.eaten ?
+      <div className="plate"></div> :
+      <div
+        className="plate"
+        onClick={handleEaten}
+        >
+        <img src={sushi.img_url} width="100%" />
       </div>
+    }
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {sushi.name} - ${sushi.price}
       </h4>
     </div>
   )
